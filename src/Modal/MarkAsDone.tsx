dd20b934d11ccdd547fd7c7b41817e85e668f99c
cygullem <crmchs.gullem.clydecyril@gmail.com>
2025-03-11 00:36:50 +0800

    media query

--- src/Modal/MarkAsDone.tsx
+++ src/Modal/MarkAsDone.tsx
@@ -11,7 +11,7 @@ const MarkAsDone: React.FC<MarkAsDoneProps> = ({ isOpen, onClose, onMarkAsDone, 
     if (!isOpen) return null;
 
     return (
-        <div className="z-[1000] fixed inset-0 flex items-center justify-center bg-opacity-30 backdrop-blur-sm">
+        <div className="home-bg z-[1000] fixed inset-0 flex items-center justify-center bg-opacity-30 backdrop-blur-sm">
             <div className="bg-[#33353C] p-6 rounded-lg shadow-xl relative w-full sm:w-3/4 md:w-1/2 lg:w-1/3">
                 <p className='text-white text-xs mb-2'>Mark as done: {item}?</p>
                 <div className='flex justify-end gap-4 mt-4'>
